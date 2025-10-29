406065b40471cd01973e547bff43960c2a3490bf
orikuOlarotimi <rotimipraise07@gmail.com>
2025-10-29 23:59:42 +0100

    l

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = new Sequelize(process.env.DATABASE_URL || DATABASE_URL , {
   dialect: "mysql",
   dialectOptions: {
     ssl: {
