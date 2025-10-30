d5570257761ee4e6d7e681aa990a1da2f2eb0d69
orikuOlarotimi <rotimipraise07@gmail.com>
2025-10-30 08:55:44 +0100

    fixed issue of not connecting to mysql

--- src/config/db.js
+++ src/config/db.js
@@ -1,18 +1,14 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-
 const sequelize = new Sequelize(
-  process.env.MYSQL_URL,
-
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
   {
+    host: process.env.DB_HOST,
     dialect: "mysql",
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false, // for Railway SSL
-      },
-    },
+    logging: false,
   }
 );
 
